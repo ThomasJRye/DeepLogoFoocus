daae7ee0ca5579d833861213c59cff559fa8acfc
thomasjrye <thomasjrye@gmail.com>
2023-02-07 13:42:40 +0100

    changed config

--- detr/datasets/__init__.py
+++ detr/datasets/__init__.py
@@ -26,3 +26,17 @@ def build_dataset(image_set, args):
         from .flickr_logos_27 import build as build_flickr_logos_27
         return build_flickr_logos_27(image_set, args)
     raise ValueError(f'dataset {args.dataset_file} not supported')
+
+def build(image_set, args):
+    root = Path(args.coco_path)
+    assert root.exists(), f'provided root path {root} does not exist'
+    train_json = 'flickr_logos_27_train.json'
+    test_json = 'flickr_logos_27_test.json'
+    PATHS = {
+        "train": (root / 'flickr_logos_27_dataset_images', root / train_json),
+        "val": (root / 'flickr_logos_27_dataset_images', root / test_json),
+    }
+
+    img_folder, ann_file = PATHS[image_set]
+    dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
+    return dataset
