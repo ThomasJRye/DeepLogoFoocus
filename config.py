b302f7f9b47a005a9bc556203afbdcefda750c42
thomasjrye <thomasjrye@gmail.com>
2023-02-10 16:48:44 +0100

    import make sense csv file

--- config.py
+++ config.py
@@ -3,10 +3,13 @@ from yacs.config import CfgNode
 
 _C = CfgNode()
 
+
 _C.DATASET_DIR = 'flickr_logos_27_dataset'
 _C.IMAGE_DIR = os.path.join(_C.DATASET_DIR, 'flickr_logos_27_dataset_images')
 _C.ANNOT_FILE = os.path.join(
     _C.DATASET_DIR, 'flickr_logos_27_dataset_training_set_annotation.txt')
+_C.ANNOT_TEST = os.path.join(
+    _C.DATASET_DIR, 'flickr_logos_27_dataset_test_set_annotation.txt')
 _C.CROPPED_ANNOT_FILE = os.path.join(
     _C.DATASET_DIR, 'flickr_logos_27_dataset_training_set_annotation_cropped.txt')
 _C.CROPPED_ANNOT_FILE_TEST = os.path.join(
@@ -16,8 +19,11 @@ _C.CLASS_NAMES = [
     'Adidas', 'Apple', 'BMW', 'Citroen', 'Cocacola', 'DHL', 'Fedex', 'Ferrari',
     'Ford', 'Google', 'HP', 'Heineken', 'Intel', 'McDonalds', 'Mini', 'Nbc',
     'Nike', 'Pepsi', 'Porsche', 'Puma', 'RedBull', 'Sprite', 'Starbucks',
-    'Texaco', 'Unicef', 'Vodafone', 'Yahoo', 'Coop'
+    'Texaco', 'Unicef', 'Vodafone', 'Yahoo', 'Coop', 'Norsk Tipping',
+    'Rema1000', 'Tess'
+
 ]
+
 
 
 def get_cfg_defaults():
